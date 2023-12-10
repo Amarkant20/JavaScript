3d22b58b7ae8ae7dc10c579d8140484576c1cef3
Amarkant20 <amarkantsonkar@gmail.com>
2023-12-10 23:08:36 +0530

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,35 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    + 3) // code readability should be high
+
+console.log("Amarkant")
+
+let name = "amarkant"
+let age = 24
+let isLogedIn = false
+let state;
+
+
+// primitive datatype
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) // null is a object
+console.log(typeof undefined)  // undefined is a dataype
+
+
+
+
+ 
